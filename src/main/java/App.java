4b7a7e88aef819ec771cdec6ae2ffa8429b1043a
Evgeniy Karapyshev <karapet2011@gmail.com>
2021-10-21 00:31:38 +0300

    Job done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat_bean1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat_bean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(cat_bean1 == cat_bean2);
     }
 }
